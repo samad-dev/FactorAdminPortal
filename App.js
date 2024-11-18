be847f1b2fd2fb0a7256564e5284cf35ebf8e6ff
samad-dev <abdulsamadq67@gmail.com>
2024-11-19 01:23:49 +0500

    port fix for live

--- App.js
+++ App.js
@@ -48,4 +48,7 @@ app.use((err, req, res, next) => {
 
 const http = require("http").createServer(app);
 
-http.listen(8000, () => console.log(`Server running on port ${8000}`));
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
